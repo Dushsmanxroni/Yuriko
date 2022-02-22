c740574c822ba8165dd1e1f51b2ba4b1adc6f18e
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-02-23 02:15:05 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -82,7 +82,7 @@ def get_readable_time(seconds: int) -> str:
 yurikorobot_IMG = "https://te.legra.ph/file/a718ff3edfc554fdfddd7.jpg"
 
 PM_START_TEXT = """
-*👋 yurikorobot_IMG, Hᴇʟʟᴏ {} !*
+*👋 yurikorobot_IMG Hᴇʟʟᴏ {} !*
 
 ✗ *I'Aᴍ Aɴ Aɴɪᴍᴇ-Tʜᴇᴍᴇ Mᴀɴᴀɢᴇᴍᴇɴᴛ Bᴏᴛ*
 ✗ *Aᴍ Vᴇʀʏ Fᴀꜱᴛ Aɴᴅ  Mᴏʀᴇ Eꜰꜰɪᴄɪᴇɴᴛ  I Pʀᴏᴠɪᴅᴇ Aᴡᴇꜱᴏᴍᴇ  Fᴇᴀᴛᴜʀᴇꜱ!*

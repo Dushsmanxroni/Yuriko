96cfd7e777281fa3a67c1def862e785900f51b75
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-02-27 11:41:34 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -232,7 +232,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_photo(f"{AMANOP_IMG}")
+            update.effective_message.reply_photo(f"AMANOP_IMG")
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
                     escape_markdown(first_name),
